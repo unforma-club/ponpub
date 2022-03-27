fa946aa389963d30ff3ff37b39019628542222f8
Taufik Oktama <oktamataufik@gmail.com>
2022-03-27 14:10:28 +0700

    `console` `feat` Create `ContextSession`

---
+++ console/components/Context/ContextSession.tsx
@@ -0,0 +1,69 @@
+import type { PropsWithChildren } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import useSWR, { KeyedMutator } from "swr";
+import fetchJson from "libs/fetchJson";
+import useIsomorphicLayout from "hooks/use-isomorphic-layout";
+
+type PayloadSession = {
+    success: boolean;
+    statusCode: number;
+    data?: any;
+};
+
+type ContextSessionAttr = {
+    initializing: boolean;
+    session: any;
+    mutateSession: KeyedMutator<PayloadSession>;
+    handleLogout(): void;
+};
+
+const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+
+const ContextSession = createContext<ContextSessionAttr>(undefined!);
+export function useSession() {
+    const session = useContext(ContextSession);
+    if (!session) throw new Error("`useSession` must be used within `ProviderSession`");
+    return session;
+}
+export const ConsumerSession = ContextSession.Consumer;
+
+export default function ProviderSession(props: PropsWithChildren<{}>) {
+    const { children } = props;
+    const { data, mutate } = useSWR<PayloadSession>("/api/v1/session/ping");
+    const [session, setSession] = useState<any>(null);
+    const [initializing, setInitializing] = useState(true);
+
+    function handleLogout() {
+        fetchJson("/api/v1/user/logout", { method: "POST" }).then(() => mutate());
+    }
+
+    const { asPath, isReady, replace } = useRouter();
+
+    useIsomorphicLayout(() => {
+        mutate()
+            .then((res) => {
+                if (res && res.success) {
+                    setSession(res.data);
+                } else {
+                    const isAuthPath = !!asPath.includes("/auth");
+                    if (!isAuthPath && isReady) {
+                        const newUrl = asPath;
+                        replace(
+                            `${NEXT_PUBLIC_API_URL}/auth?callback_url=${NEXT_PUBLIC_SITE_URL}${newUrl}`
+                        );
+                    }
+                }
+            })
+            .then(() => setInitializing(false));
+    }, [data, asPath, isReady, mutate, replace]);
+
+    return (
+        <ContextSession.Provider
+            value={{ initializing, session, mutateSession: mutate, handleLogout }}
+        >
+            {children}
+        </ContextSession.Provider>
+    );
+}
