df5374c6a3f1c51f54893f6bf60ba8ba55d8387a
Taufik Oktama <oktamataufik@gmail.com>
2022-03-27 14:02:36 +0700

    `service` `feat` Create session issuer

---
+++ service/src/services/service.session.ts
@@ -0,0 +1,13 @@
+import type { Request } from "express";
+import type { BaseUser } from "types/user";
+import { jwtSign } from "utils/util.jwt";
+
+const ServiceSession = {
+    issueSession: function (req: Request, user: BaseUser) {
+        req.session = {
+            jwt: jwtSign(user)
+        };
+    }
+};
+
+export default ServiceSession;
