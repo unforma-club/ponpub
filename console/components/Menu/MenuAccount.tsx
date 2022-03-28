265a8ef2da714aca694f73b4c988b4827f55db5a
Taufik Oktama <oktamataufik@gmail.com>
2022-03-28 08:31:34 +0700

    `console` `fix` Initial value to `false`

--- console/components/Menu/MenuAccount.tsx
+++ console/components/Menu/MenuAccount.tsx
@@ -4,7 +4,7 @@ import { useSession } from "components/Context/ContextSession";
 
 export default function MenuAccount() {
     const { handleLogout } = useSession();
-    const { value, toggle } = useBoolean(true, true);
+    const { value, toggle } = useBoolean(false, true);
     return (
         <UCTree defaultOpen={value} parent={<button onClick={toggle}>Account</button>}>
             <button onClick={handleLogout}>Logout</button>
