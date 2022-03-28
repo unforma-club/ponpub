ca91b4920153f62f1023bfcf263134eec1cd5d17
Taufik Oktama <oktamataufik@gmail.com>
2022-03-28 08:31:52 +0700

    `console` `fix` Clean up `jsx`

--- console/pages/_app.tsx
+++ console/pages/_app.tsx
@@ -1,5 +1,4 @@
 import "styles/global.scss";
-// import { NextPage } from "next";
 import { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import { ThemeProvider as ProviderTheme } from "next-themes";
@@ -8,32 +7,24 @@ import ProviderSession from "components/Context/ContextSession";
 import SessionGuard from "components/SessionGuard";
 import AppAside from "components/AppAside";
 
-// type CustomAppProps = AppProps & {
-//     Component: NextPage & {
-//         requireAuth?: boolean;
-//     };
-// };
-
 export default function CustomApp(props: AppProps) {
     const { Component, pageProps } = props;
     return (
-        <>
-            <SWRConfig value={{ fetcher: fetchJson, loadingTimeout: 10000 }}>
-                <ProviderTheme
-                    defaultTheme="system"
-                    themes={["dark", "light"]}
-                    disableTransitionOnChange
-                    enableColorScheme
-                    enableSystem
-                >
-                    <ProviderSession>
-                        <SessionGuard>
-                            <AppAside />
-                            <Component {...pageProps} />
-                        </SessionGuard>
-                    </ProviderSession>
-                </ProviderTheme>
-            </SWRConfig>
-        </>
+        <SWRConfig value={{ fetcher: fetchJson, loadingTimeout: 10000 }}>
+            <ProviderTheme
+                defaultTheme="system"
+                themes={["dark", "light"]}
+                disableTransitionOnChange
+                enableColorScheme
+                enableSystem
+            >
+                <ProviderSession>
+                    <SessionGuard>
+                        <AppAside />
+                        <Component {...pageProps} />
+                    </SessionGuard>
+                </ProviderSession>
+            </ProviderTheme>
+        </SWRConfig>
     );
 }
