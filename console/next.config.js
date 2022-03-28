1a45501b5595d056d7cdbe42ec0aed56aa4f89ff
Taufik Oktama <oktamataufik@gmail.com>
2022-03-28 08:48:33 +0700

    `console` `fix` Remove `basePath`

--- console/next.config.js
+++ console/next.config.js
@@ -8,7 +8,6 @@ module.exports = {
             16, 32, 48, 64, 128, 256, 384, 512, 720, 960, 1280, 1366, 1536, 1920, 2200, 2560
         ]
     },
-    basePath: "/ponpub",
     async rewrites() {
         return [
             {
