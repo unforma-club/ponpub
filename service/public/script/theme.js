018f753a5e6a409b7015b418aab92bb563190ec8
Taufik Oktama <oktamataufik@gmail.com>
2022-03-28 22:10:00 +0700

    `fix` Resolve `system` theme

--- service/public/script/theme.js
+++ service/public/script/theme.js
@@ -3,6 +3,9 @@
     const storedTheme = localStorage.getItem("theme");
     const attr = "data-theme";
 
+    const MEDIA = "(prefers-color-scheme: dark)";
+    const matchesDark = window.matchMedia(MEDIA).matches;
+
     switch (storedTheme) {
         case "dark":
             el.setAttribute(attr, "dark");
@@ -11,7 +14,7 @@
             el.setAttribute(attr, "light");
             break;
         default:
-            el.setAttribute(attr, "light");
+            el.setAttribute(attr, matchesDark ? "dark" : "light");
             break;
     }
 })();
