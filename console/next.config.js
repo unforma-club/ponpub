836a5d42b37e0dbcd976b4407fb64a99a0c6944a
Taufik Oktama <oktamataufik@gmail.com>
2022-03-28 08:29:52 +0700

    `console` `feat` Add `basePath`

--- console/next.config.js
+++ console/next.config.js
@@ -8,6 +8,7 @@ module.exports = {
             16, 32, 48, 64, 128, 256, 384, 512, 720, 960, 1280, 1366, 1536, 1920, 2200, 2560
         ]
     },
+    basePath: "/ponpub",
     async rewrites() {
         return [
             {
