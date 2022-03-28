f063b404f65dc70db302a8ea98a7136fe7c50705
Taufik Oktama <oktamataufik@gmail.com>
2022-03-28 22:10:38 +0700

    `fix` Resolve `jwt` expired that return `400`

--- service/src/middlewares/mid.session.ts
+++ service/src/middlewares/mid.session.ts
@@ -4,13 +4,14 @@ import { jwtVerify } from "utils/util.jwt";
 const MidSession = {
     currentUser: function (req: Request, res: Response, next: NextFunction) {
         if (!req.session.jwt) return next();
-        const session = jwtVerify(req.session.jwt);
-        req.currentUser = session;
+        try {
+            const session = jwtVerify(req.session.jwt);
+            req.currentUser = session;
+        } catch (error) {}
         return next();
     },
     ping: function (req: Request, res: Response, next: NextFunction) {
         if (!req.session.jwt) return res.status(200).json({ success: false, statusCode: 200 });
-
         const session = jwtVerify(req.session.jwt);
         req.currentUser = session;
         return next();
