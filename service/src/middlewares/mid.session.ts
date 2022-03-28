1c4ce67bd45eb488e39300d7903e68b94e13f44b
Taufik Oktama <oktamataufik@gmail.com>
2022-03-28 15:20:13 +0700

    `feat` Create `currentUser` middleware

--- service/src/middlewares/mid.session.ts
+++ service/src/middlewares/mid.session.ts
@@ -2,6 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import { jwtVerify } from "utils/util.jwt";
 
 const MidSession = {
+    currentUser: function (req: Request, res: Response, next: NextFunction) {
+        if (!req.session.jwt) return next();
+        const session = jwtVerify(req.session.jwt);
+        req.currentUser = session;
+        return next();
+    },
     ping: function (req: Request, res: Response, next: NextFunction) {
         if (!req.session.jwt) return res.status(200).json({ success: false, statusCode: 200 });
 
