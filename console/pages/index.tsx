156a8e058fda8d9af40b0185aaf514ad0ef63f67
Taufik Oktama <oktamataufik@gmail.com>
2022-03-29 23:43:44 +0700

    `feat` Routing without domain

--- console/pages/index.tsx
+++ console/pages/index.tsx
@@ -1,21 +1,11 @@
 import { useRouter } from "next/router";
 
-const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
-
 export default function Page() {
-    const { asPath } = useRouter();
+    const { asPath, basePath } = useRouter();
     return (
         <div>
             Page Index
-            <button
-                onClick={() =>
-                    window.location.replace(
-                        `/api/auth?callback_url=${NEXT_PUBLIC_SITE_URL}/ponpub${asPath}`
-                    )
-                }
-            >
-                To Auth
-            </button>
+            <a href={`/api/auth?callback_url=${basePath}${asPath}`}>To Auth</a>
         </div>
     );
 }
