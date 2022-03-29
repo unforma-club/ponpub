a00a198a20ee97892f8ab27f46cb4228785bb77d
Taufik Oktama <oktamataufik@gmail.com>
2022-03-29 23:34:01 +0700

    `fix` Improve fetching session

--- console/components/Context/ContextSession.tsx
+++ console/components/Context/ContextSession.tsx
@@ -1,9 +1,7 @@
 import type { PropsWithChildren } from "react";
-import { createContext, useContext, useState } from "react";
-import { useRouter } from "next/router";
+import { createContext, useContext, useEffect, useState } from "react";
 import useSWR, { KeyedMutator } from "swr";
 import fetchJson from "libs/fetchJson";
-import useIsomorphicLayout from "hooks/use-isomorphic-layout";
 
 type PayloadSession = {
     success: boolean;
@@ -18,8 +16,6 @@ type ContextSessionAttr = {
     handleLogout(): void;
 };
 
-const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
-
 const ContextSession = createContext<ContextSessionAttr>(undefined!);
 export function useSession() {
     const session = useContext(ContextSession);
@@ -31,34 +27,28 @@ export const ConsumerSession = ContextSession.Consumer;
 export default function ProviderSession(props: PropsWithChildren<{}>) {
     const { children } = props;
     const { data, mutate } = useSWR<PayloadSession>("/api/v1/session/ping");
-    const [session, setSession] = useState<any>(null);
+    const [session, setSession] = useState<any>(data?.data);
     const [initializing, setInitializing] = useState(true);
 
     function handleLogout() {
         fetchJson(`/api/v1/user/logout`, { method: "POST" }).then(() => window.location.reload());
     }
 
-    const { asPath, isReady } = useRouter();
-
-    useIsomorphicLayout(() => {
+    useEffect(() => {
         mutate()
             .then((res) => {
                 if (res && res.success) {
                     setSession(res.data);
                 } else {
-                    const newUrl = asPath;
-
                     // Remember that we're set `basePath` as `/ponpub`
                     // It's important to use `window` object instead of `useRouter()`
                     // Forcing url to be `https://domain.com/api/*`
                     // If we use `useRouter()` the url will be `https://domain.com/ponpub/api/*`
-                    window.location.replace(
-                        `/api/auth?callback_url=${NEXT_PUBLIC_SITE_URL}/ponpub${newUrl}`
-                    );
+                    window.location.replace(`/api/auth?callback_url=${window.location.href}`);
                 }
             })
             .then(() => setInitializing(false));
-    }, [data, asPath, isReady, mutate]);
+    }, [data, mutate, setSession, setInitializing]);
 
     return (
         <ContextSession.Provider
