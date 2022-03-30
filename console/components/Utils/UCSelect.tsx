1fb492e357c6b656fb503e6839969396febef7f4
Taufik Oktama <oktamataufik@gmail.com>
2022-03-30 14:16:44 +0700

    `feat` Semantic react properties

--- console/components/Utils/UCSelect.tsx
+++ console/components/Utils/UCSelect.tsx
@@ -1,29 +1,20 @@
 import styles from "styles/uc-select.module.scss";
-import type { CSSProperties } from "react";
+import type { DetailedHTMLProps, SelectHTMLAttributes } from "react";
 
-export type UCSelectProps = {
-    name: string;
+interface NewProps
+    extends DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement> {
     items: string[];
-    value: string;
-    onChange(v: string): void;
-    style?: CSSProperties;
-    className?: string;
-};
+}
 
-export default function UCSelect(props: UCSelectProps) {
-    const { name, items, value, onChange, style, className } = props;
+export default function UCSelect(props: NewProps) {
+    const { name, items, style, className, ...rest } = props;
     return (
         <label
             title={name}
             style={style}
             className={className ? `${styles.container} ${className}` : styles.container}
         >
-            <select
-                className={styles.select}
-                inputMode="text"
-                value={value}
-                onChange={(e) => onChange(e.target.value)}
-            >
+            <select className={styles.select} name={name} {...rest}>
                 {items.map((item, i) => (
                     <option key={i}>{item}</option>
                 ))}
@@ -33,8 +24,8 @@ export default function UCSelect(props: UCSelectProps) {
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
-                    height="1em"
-                    width="1em"
+                    height="1.1em"
+                    width="1.1em"
                     fill="currentColor"
                 >
                     <path d="M12 5.83L15.17 9l1.41-1.41L12 3 7.41 7.59 8.83 9 12 5.83zm0 12.34L8.83 15l-1.41 1.41L12 21l4.59-4.59L15.17 15 12 18.17z" />
