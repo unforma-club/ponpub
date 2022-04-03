870b723ff457b87e1eee59876107e97c9d35c7af
Taufik Oktama <oktamataufik@gmail.com>
2022-04-04 03:52:55 +0700

    `feat` Check wether is server or browser

--- console/libs/fetchJson.ts
+++ console/libs/fetchJson.ts
@@ -2,7 +2,9 @@ export default async function fetchJson<JSON = unknown>(
     input: RequestInfo,
     init?: RequestInit
 ): Promise<JSON> {
-    const response = await fetch(input, init);
+    const NEXT_PUBLIC_ORIGIN = process.env.NEXT_PUBLIC_ORIGIN;
+    const isServer = typeof window === "undefined";
+    const response = await fetch(`${isServer ? `${NEXT_PUBLIC_ORIGIN}${input}` : input}`, init);
     const data = await response.json();
     return data;
 }
