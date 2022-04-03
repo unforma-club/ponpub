bc570b9c851a74b7192091c4aa630a97c46c2ac9
Taufik Oktama <oktamataufik@gmail.com>
2022-04-04 03:58:45 +0700

    `feat` Clean up

--- console/next.config.js
+++ console/next.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     reactStrictMode: true,
     poweredByHeader: false,
+    basePath: "/ponpub",
+    experimental: {
+        outputStandalone: true
+    },
     images: {
         imageSizes: [
             16, 32, 48, 64, 128, 256, 384, 512, 720, 960, 1280, 1366, 1536, 1920, 2200, 2560
         ]
-    },
-    basePath: "/ponpub"
+    }
 };
