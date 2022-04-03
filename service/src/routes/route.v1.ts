e5032325528c0a0b446abc0232a17ac23ca7909e
Taufik Oktama <oktamataufik@gmail.com>
2022-04-04 03:20:51 +0700

    `feat` `Post` routes

--- service/src/routes/route.v1.ts
+++ service/src/routes/route.v1.ts
@@ -3,6 +3,7 @@ import FieldValidation from "middlewares/mid.field";
 import MidSession from "middlewares/mid.session";
 import ControlUser from "controllers/control.user";
 import ControlSession from "controllers/control.session";
+import ControlPost from "controllers/control.post";
 
 const RouterV1 = Router();
 
@@ -11,5 +12,8 @@ RouterV1.get("/session/ping", MidSession.ping, ControlSession.me);
 RouterV1.post("/user/login", ControlUser.login);
 RouterV1.post("/user/logout", ControlUser.logout);
 RouterV1.post("/user/register", FieldValidation.register, ControlUser.register);
+
+RouterV1.route("/post").post(ControlPost.create).get(ControlPost.getPosts);
+RouterV1.route("/post/:slug").get(ControlPost.getPostBySlug);
 
 export default RouterV1;
