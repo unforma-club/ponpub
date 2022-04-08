dc05eb054b08f2493db9a28b5bfcd6014f2402db
Taufik Oktama <oktamataufik@gmail.com>
2022-04-09 01:25:39 +0700

    `feat` Create new routes

--- service/src/routes/route.v1.ts
+++ service/src/routes/route.v1.ts
@@ -4,6 +4,8 @@ import MidSession from "middlewares/mid.session";
 import ControlUser from "controllers/control.user";
 import ControlSession from "controllers/control.session";
 import ControlPost from "controllers/control.post";
+import ControlSetting from "controllers/control.setting";
+import ControlAsset from "controllers/control.asset";
 
 const RouterV1 = Router();
 
@@ -14,6 +16,17 @@ RouterV1.post("/user/logout", ControlUser.logout);
 RouterV1.post("/user/register", FieldValidation.register, ControlUser.register);
 
 RouterV1.route("/post").post(ControlPost.create).get(ControlPost.getPosts);
-RouterV1.route("/post/:slug").get(ControlPost.getPostBySlug);
+RouterV1.route("/post/:slug")
+    .get(ControlPost.getPostBySlug)
+    .patch(ControlPost.patchPostBySlug)
+    .delete(ControlPost.deletePostBySlug);
+
+RouterV1.route("/admin/user").get(ControlUser.getUser);
+RouterV1.route("/admin/setting/site")
+    .get(ControlSetting.getSiteData)
+    .post(ControlSetting.postSiteDate)
+    .patch(ControlSetting.patchSiteData);
+
+RouterV1.route("/content/images/logo").post(ControlAsset.storageLogo, ControlAsset.uploadLogo);
 
 export default RouterV1;
