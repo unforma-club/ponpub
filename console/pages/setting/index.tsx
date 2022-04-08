fca586ec5ffea156f2121d97ee203234e5e72f87
Taufik Oktama <oktamataufik@gmail.com>
2022-04-09 01:50:15 +0700

    `feat` Create page index setting

---
+++ console/pages/setting/index.tsx
@@ -0,0 +1,11 @@
+import AppHeader from "components/AppHeader";
+import LayoutMain from "components/Layout/LayoutMain";
+
+export default function Page() {
+    return (
+        <LayoutMain>
+            <AppHeader />
+            Setting / Index
+        </LayoutMain>
+    );
+}
