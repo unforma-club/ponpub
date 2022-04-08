36f1e69c28b507f68b9878c1a48de754814c5481
Taufik Oktama <oktamataufik@gmail.com>
2022-04-09 01:40:55 +0700

    `feat` Use `AppGuard`

--- console/pages/_app.tsx
+++ console/pages/_app.tsx
@@ -4,12 +4,10 @@ import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { SWRConfig } from "swr";
 import { ThemeProvider as ProviderTheme } from "next-themes";
-import NextHead from "next/head";
 import nProgress from "nprogress";
 import fetchJson from "libs/fetchJson";
-import ProviderSession from "components/Context/ContextSession";
-import SessionGuard from "components/SessionGuard";
 import AppAside from "components/AppAside";
+import AppGuard from "components/AppGuard";
 
 nProgress.configure({
     showSpinner: true,
@@ -37,11 +35,9 @@ export default function CustomApp(props: AppProps) {
             events.off("routeChangeError", handleStop);
         };
     }, [events]);
+
     return (
         <>
-            <NextHead>
-                <title>Ponpub</title>
-            </NextHead>
             <SWRConfig value={{ fetcher: fetchJson, loadingTimeout: 10000 }}>
                 <ProviderTheme
                     storageKey="ponpub-theme"
@@ -51,12 +47,10 @@ export default function CustomApp(props: AppProps) {
                     enableColorScheme
                     enableSystem
                 >
-                    <ProviderSession>
-                        <SessionGuard>
-                            <AppAside />
-                            <Component {...pageProps} />
-                        </SessionGuard>
-                    </ProviderSession>
+                    <AppGuard>
+                        <AppAside />
+                        <Component {...pageProps} />
+                    </AppGuard>
                 </ProviderTheme>
             </SWRConfig>
         </>
